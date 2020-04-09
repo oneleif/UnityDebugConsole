6d86da9c856ee8cb934484bd3283e7978122622e
fuhmac <fuhrmann.maciej@gmail.com>
2020-04-09 20:33:06 +0200

    Renaming and new implementation of developer console

---
+++ Runtime/Scripts/Commands/CommandQuit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Oneleif.debugconsole
+{
+    public class CommandQuit : ConsoleCommand
+    {
+        public override string Name { get; protected set; }
+        public override string Command { get; protected set; }
+        public override string Description { get; protected set; }
+        public override string Help { get; protected set; }
+
+        public CommandQuit()
+        {
+            Name = "Quit";
+            Command = "quit";
+            Description = "Quits the application";
+            Help = "Use this command with no arguments to force Unity to quit!";
+
+            AddCommandToConsole();
+        }
+
+        public override void RunCommand()
+        {
+            if (Application.isEditor)
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+
+        public static CommandQuit CreateCommand()
+        {
+            return new CommandQuit();
+        }
+    }
+}
---
+++ Runtime/Scripts/DeveloperConsole.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Oneleif.debugconsole
+{
+    public abstract class ConsoleCommand
+    {
+        public abstract string Name { get; protected set; }
+        public abstract string Command { get; protected set; }
+        public abstract string Description { get; protected set; }
+        public abstract string Help { get; protected set; }
+
+        public void AddCommandToConsole()
+        {
+            DeveloperConsole.AddCommandsToConsole(Command, this);
+        }
+
+        public abstract void RunCommand();
+    }
+
+    public class DeveloperConsole : MonoBehaviour
+    {
+        public static DeveloperConsole Instance { get; private set; }
+        public static Dictionary<string, ConsoleCommand> Commands { get; private set; }
+
+        [Header("UI Components")]
+        public Canvas consoleCanvas;
+
+        public Text consoleText;
+        public Text inputText;
+        public InputField consoleInput;
+
+        private void Awake()
+        {
+            if (FindObjectsOfType(GetType()).Length > 1)
+            {
+                Destroy(gameObject);
+            }
+
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance == this)
+            {
+                Destroy(Instance.gameObject);
+                Instance = this;
+            }
+            DontDestroyOnLoad(this.gameObject);
+            Commands = new Dictionary<string, ConsoleCommand>();
+        }
+
+        private void Start()
+        {
+            consoleCanvas.gameObject.SetActive(false);
+            AddMessageToConsole("Starting Developer Console...");
+            CreateCommands();
+        }
+
+        private void OnEnable()
+        {
+            Application.logMessageReceived += HandleLog;
+        }
+
+        private void OnDisable()
+        {
+            Application.logMessageReceived -= HandleLog;
+        }
+
+        private void HandleLog(string logMessage, string stackTrace, LogType type)
+        {
+            string message = string.Empty;
+            if (type.Equals(LogType.Warning))
+            {
+                message = "<color=yellow>[" + type.ToString() + "]" + logMessage + "</color> ";
+            }
+            else
+            {
+                message = "<color=white>[" + type.ToString() + "]" + logMessage + "</color> ";
+            }
+            AddMessageToConsole(message);
+        }
+
+        private void CreateCommands()
+        {
+            CommandQuit.CreateCommand();
+        }
+
+        public static void AddCommandsToConsole(string name, ConsoleCommand command)
+        {
+            if (!Commands.ContainsKey(name))
+            {
+                Commands.Add(name, command);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.BackQuote))
+            {
+                consoleCanvas.gameObject.SetActive(!consoleCanvas.gameObject.activeInHierarchy);
+            }
+
+            if (consoleCanvas.gameObject.activeInHierarchy)
+            {
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    if (inputText.text != "")
+                    {
+                        AddMessageToConsole(inputText.text);
+                        ParseInput(inputText.text);
+                    }
+                }
+            }
+        }
+
+        private void AddMessageToConsole(string msg)
+        {
+            consoleText.text += msg + "\n";
+        }
+
+        private void ParseInput(string parseInput)
+        {
+            string[] input = parseInput.Split(null);
+
+            if (input.Length == 0 || input == null)
+            {
+                Debug.LogWarning("Command not recognized.");
+                return;
+            }
+
+            if (!Commands.ContainsKey(input[0]))
+            {
+                Debug.LogWarning("Command not recognized.");
+            }
+            else
+            {
+                Commands[input[0]].RunCommand();
+            }
+        }
+    }
+}
